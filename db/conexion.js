afe305b49c75760d0fa529e96f3de423e8403b23
unknown <ronaldperera85@gmail.com>
2025-02-19 16:57:37 -0400

    db_chrome

--- db/conexion.js
+++ db/conexion.js
@@ -1,15 +1,15 @@
-const mysql = require('mysql2/promise');
-require('dotenv').config(); // Si estás usando dotenv para variables de entorno
+const mysql = require("mysql2/promise");
+require("dotenv").config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'whatsapp_app',
-    port: process.env.DB_PORT || 3306,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USERNAME || "root",
+  password: process.env.DB_PASSWORD || "",
+  database: process.env.DB_DATABASE || "whatsapp_app",
+  port: process.env.DB_PORT || 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
 // Función para realizar consultas a la base de datos
@@ -20,16 +20,16 @@ const query = async (sql, values) => {
     const [results] = await connection.execute(sql, values);
     return results;
   } catch (error) {
-    console.error('Error executing query:', error);
-    throw error; // Re-lanza el error para que pueda ser manejado en el controlador
+    console.error("Error executing query:", error);
+    throw error;
   } finally {
     if (connection) {
-        connection.release(); // Liberar la conexión de vuelta al pool
+      connection.release();
     }
   }
 };
 
 module.exports = {
   query,
-    pool,
+  pool,
 };
